303ab935474e0c53b341a1db6e44140bb5f689f9
Michelle Naffate <michelle.naffate205164@potros.itson.edu.mx>
2021-02-21 23:19:48 -0700

    Practica4

---
+++ app/src/main/java/naffate/mcihelle/practica4/MenuActivity.kt
@@ -0,0 +1,11 @@
+package naffate.mcihelle.practica4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
---
+++ app/src/main/java/naffate/mcihelle/practica4/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package naffate.mcihelle.practica4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        val button:Button = findViewById(R.id.button_register)
+        button.setOnClickListener {
+            var intent: Intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
